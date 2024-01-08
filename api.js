fd09ac4977bac5980f56f77c50dae9a75e01ac42
akuridho220 <akuridho220@gmail.com>
2024-01-08 17:07:04 +0700

    update endpoint daftar sampel

--- api.js
+++ api.js
@@ -22,34 +22,6 @@ client.connect((err) => {
 });
 
 // Endpoint here
-app.get('/tes', (req, res) => {
-  client.query(
-    `
-        SELECT
-            rumahtangga.no_bs AS kode_bs,
-            MIN(posisi_pcl.nim) AS nim,
-            MIN(mahasiswa.nama) AS nama_pcl,
-            COUNT(*) AS jumlah_listing
-        FROM
-            rumahtangga
-        LEFT JOIN bloksensus ON bloksensus.no_bs = rumahtangga.no_bs
-        LEFT JOIN posisi_pcl ON posisi_pcl.nim = bloksensus.nim_pencacah
-        LEFT JOIN mahasiswa ON mahasiswa.nim = bloksensus.nim_pencacah
-        GROUP BY
-            rumahtangga.no_bs
-        ORDER BY
-            rumahtangga.no_bs DESC
-        `,
-    (err, result) => {
-      if (!err) {
-        res.send(result.rows);
-      } else {
-        console.log(err.message);
-      }
-    }
-  );
-});
-
 // ENDPOINT FOR DASHBOARD
 app.get('/api/dashboard/total-listing', (req, res) => {
   client.query(
@@ -184,6 +156,149 @@ app.get('/api/riset/daftar/listing/kab', (req, res) => {
 });
 
 // - keseluruhan
+//Detail Listing
+// - bloksensus
+// - desa
+// - kecamatan
+// - kabupaten
+
+
+// Daftar Sampel
+// - bloksensus
+app.get('/api/riset/daftar/sampel/bs', (req, res) => {
+    client.query(
+        `
+        SELECT
+            datast.no_bs AS kode_bs,
+            MIN(datast.kode_ruta) AS kode_ruta,
+            MIN(posisi_pcl.nim) AS nim,
+            MIN(mahasiswa.nama) AS nama_pcl,
+            COUNT(*) AS jumlah_sampel
+        FROM
+            datast
+        LEFT JOIN rumahtangga ON rumahtangga.kode_ruta = datast.kode_ruta
+        LEFT JOIN bloksensus ON bloksensus.no_bs = rumahtangga.no_bs
+        LEFT JOIN posisi_pcl ON posisi_pcl.nim = bloksensus.nim_pencacah
+        LEFT JOIN mahasiswa ON posisi_pcl.nim = mahasiswa.nim
+        GROUP BY
+            kode_bs
+        ORDER BY
+            kode_bs DESC
+        `,
+        (err, result) => {
+            if (!err) {
+                res.send(result.rows);
+            } else {
+                console.log(err.message);
+            }
+        }
+    );
+});
+
+// - kecamatan
+app.get('/api/riset/daftar/sampel/kec', (req, res) => {
+    client.query(
+        `
+        SELECT
+            MIN(datast.no_bs) AS kode_bs,
+            kecamatan.id_kec AS kode_kecamatan,
+            MIN(kecamatan.nama_kec) AS nama_kecamatan,
+            COUNT(*) AS jumlah_sampel
+        FROM
+            datast
+        LEFT JOIN
+            rumahtangga ON rumahtangga.kode_ruta = datast.kode_ruta
+        LEFT JOIN
+            bloksensus ON bloksensus.no_bs = rumahtangga.no_bs
+        LEFT JOIN
+            kecamatan ON kecamatan.id_kec = bloksensus.id_kec
+        GROUP BY
+            kecamatan.id_kec
+        ORDER BY
+            kecamatan.id_kec DESC
+        `,
+        (err, result) => {
+            if (!err) {
+                res.send(result.rows);
+            } else {
+                console.log(err.message);
+            }
+        }
+    );
+});
+
+// - kabupaten
+app.get('/api/riset/daftar/sampel/kab', (req, res) => {
+    client.query(
+        `
+        SELECT
+            MIN(datast.no_bs) AS kode_bs,
+            kabupaten.id_kab AS kode_kabupaten,
+            MIN(kabupaten.nama_kab) AS nama_kabupaten,
+            COUNT(*) AS jumlah_sampel
+        FROM
+            datast
+        LEFT JOIN
+            rumahtangga ON rumahtangga.kode_ruta = datast.kode_ruta
+        LEFT JOIN
+            bloksensus ON bloksensus.no_bs = rumahtangga.no_bs
+        LEFT JOIN
+            kabupaten ON kabupaten.id_kab = bloksensus.id_kab
+        GROUP BY
+            kabupaten.id_kab
+        ORDER BY
+            kabupaten.id_kab DESC
+        `,
+        (err, result) => {
+            if (!err) {
+                res.send(result.rows);
+            } else {
+                console.log(err.message);
+            }
+        }
+    );
+});
+
+// - desa / kelurahan
+app.get('/api/riset/daftar/sampel/desa', (req, res) => {
+    client.query(
+        `
+        SELECT
+            MIN(datast.no_bs) AS kode_bs,
+            kelurahan.id_kelurahan AS kode_kelurahan,
+            MIN(kelurahan.nama_kelurahan) AS nama_kelurahan,
+            COUNT(*) AS jumlah_sampel
+        FROM
+            datast
+        LEFT JOIN
+            rumahtangga ON rumahtangga.kode_ruta = datast.kode_ruta
+        LEFT JOIN
+            bloksensus ON bloksensus.no_bs = rumahtangga.no_bs
+        LEFT JOIN
+            kelurahan ON kelurahan.id_kelurahan = bloksensus.id_kelurahan
+        GROUP BY
+            kelurahan.id_kelurahan
+        ORDER BY
+            kelurahan.id_kelurahan DESC
+        `,
+        (err, result) => {
+            if (!err) {
+                res.send(result.rows);
+            } else {
+                console.log(err.message);
+            }
+        }
+    );
+});
+
+
+// Daftar Pertim
+// Listing
+
+
+// Sampel
+
+
 
 // Endpoint for Monitoring PCL
 app.get('/api/monitoring-pcl', (req, res) => {
