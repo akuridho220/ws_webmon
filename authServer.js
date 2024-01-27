5c39875d5a3a54140a73bdb2e460caf390feec51
Alief Zulvan <depanb187@gmail.com>
2024-01-27 14:05:04 +0700

    web service add forgot password api done

--- authServer.js
+++ authServer.js
@@ -23,18 +23,18 @@ authClient.connect((err) => {
 });
 
 // REGISTER A USER
-// app.post('/api/createUser', async (req, res) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//     const query = 'INSERT INTO users (name, password, email, jenis, jabatan) VALUES ($1, $2, $3, $4, $5)';
-//     const values = [req.body.name, hashedPassword, req.body.email, req.body.jenis, req.body.jabatan];
-//     await authClient.query(query, values);
-//     res.status(201).send('User created successfully');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error creating user');
-//   }
-// });
+app.post('/api/createUser', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const query = 'INSERT INTO users (name, password, email, jenis, jabatan) VALUES ($1, $2, $3, $4, $5)';
+    const values = [req.body.name, hashedPassword, req.body.email, req.body.jenis, req.body.jabatan];
+    await authClient.query(query, values);
+    res.status(201).send('User created successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating user');
+  }
+});
 
 // Declare an array to store active access tokens
 
@@ -72,6 +72,16 @@ function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
 }
 
+// Password reset token
+async function generatePasswordResetToken() {
+  var randomstring = require('randomstring');
+  const randomToken = randomstring.generate({
+    length: 50,
+    charset: 'alphabetic',
+  });
+  return randomToken;
+}
+
 app.delete('/api/logout', async (req, res) => {
   const tokenToInvalidate = req.body.token;
   if (!tokenToInvalidate) {
@@ -85,5 +95,54 @@ app.delete('/api/logout', async (req, res) => {
     res.status(202).send('Logged out!');
   } else {
     res.status(401).send('Invalid token');
+  }
+});
+
+app.put('/api/createPasswordReset', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM users WHERE email = $1';
+    const result = await authClient.query(query, [req.body.email]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).send('User tidak terdaftar dalam database');
+    }
+
+    const passwordResetToken = await generatePasswordResetToken();
+    try {
+      const updateQuery = `UPDATE users SET password_reset_token = $1 WHERE email = $2`;
+      await authClient.query(updateQuery, [passwordResetToken, req.body.email]);
+      res.status(200).send('Password reset token berhasil diubah');
+    } catch {
+      res.status(401).send('Reset token telah diambil harap coba lagi');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during send password reset');
+  }
+});
+
+app.put('/api/resetPassword', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM users WHERE password_reset_token = $1';
+    const result = await authClient.query(query, [req.body.token]);
+    const user = result.rows[0];
+    const email = user.email;
+
+    if (!user) {
+      return res.status(404).send('User tidak terdaftar dalam database');
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    try {
+      const updateQuery = `UPDATE users SET password = $1, password_reset_at = NOW(), password_reset_token = NULL WHERE email = $2`;
+      await authClient.query(updateQuery, [hashedPassword, email]);
+      res.status(200).send('Password berhasil diubah');
+    } catch {
+      res.status(401).send('Gagal mengubah password');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during password reset');
   }
 });
